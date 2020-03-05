ff16f3268254500659970717de8657d931bd22b3
Mikkel Broberg <mb@webhouse.dk>
2020-03-05 08:39:41 +0100

    api test added

---
+++ mysql/mysql_handler.js
@@ -0,0 +1,17 @@
+
+const mysql = require('mysql2');
+
+const { DB_HOST, DB_USER, DATABASE, PASSWORD } = process.env
+
+let connection = mysql.createPool({
+	host: DB_HOST,
+	user: DB_USER,
+	password: PASSWORD,
+	database: DATABASE,
+	multipleStatements: true,
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0
+})
+connection = connection.promise()
+module.exports = connection
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const helmet = require('helmet')
 const app = express()
 
 // API endpoint imports
+const test = require('./api/index')
 
 const port = process.env.NODE_PORT || 3029
 
@@ -19,6 +20,8 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(cors())
 
+//---API---------------------------------------
+app.use([test])
 
 //---Start the express server---------------------------------------------------
 
@@ -35,3 +38,13 @@ const startServer = () => {
 }
 
 startServer()
+
+const mysqlConn = require('./mysql/mysql_handler')
+
+async function testsql() {
+	let select = `SELECT * FROM test`
+	let rs = await mysqlConn.query(select, [])
+	console.log(rs[0])
+}
+
+testsql()
