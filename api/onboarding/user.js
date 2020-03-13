9b49188881ce324682f6aa672eaa3a89612ce638
Mikkel Broberg <mb@webhouse.dk>
2020-03-13 13:39:31 +0100

    ww user device fix step 5 - fix removed

--- api/onboarding/user.js
+++ api/onboarding/user.js
@@ -26,60 +26,58 @@ const aclClient = require('../../server').aclClient
 const mysqlConn = require('../../mysql/mysql_handler')
 const InstallationInfo = require('./dataClasses/InstallationInfo')
 
-router.get('/onboarding/userfix/:uuid', async (req, res) => {
-	if (authClient.getStoredToken() === false) {
-		let login = await authClient.login(process.env.SENTIUSER, process.env.SENTIPASS)
-		authClient.setStoredToken(login.token)
-	}
-	if (await authClient.getTokenLease(authClient.getStoredToken()) === false) {
-		authClient.setStoredToken((await authClient.login(process.env.SENTIUSER, process.env.SENTIPASS)).token)
-	}
-	coreAPI.setHeader('Authorization', 'Bearer ' + authClient.getStoredToken())
-	dataBrokerAPI.setHeader('Authorization', 'Bearer ' + authClient.getStoredToken())
-	if (req.headers['wlhost']) {
-		console.log('SET WLHOST', req.headers['wlhost'])
-		coreAPI.setHeader('wlhost', req.headers['wlhost'])
-		dataBrokerAPI.setHeader('wlhost', req.headers['wlhost'])
-	}
-	let userPost = await coreAPI.get(`/v2/entity/users/${req.params.uuid}`)
-	if(userPost.ok === false) {
-		res.status(userPost.status).json()
-		return
-	}
-	let users = []
-	await userPost.data.reduce(async (promise, user) => {
-		// This line will wait for the last async function to finish.
-		// The first iteration uses an already resolved Promise
-		// so, it will immediately continue.
-		await promise;
-		console.log(user.uuid)
-		let internalPost = await coreAPI.get(`/v2/entity/user/${user.uuid}/internal`)
-		if(internalPost.ok === false) {
-			res.status(internalPost.status).json()
-			return
-		}
-		console.log(internalPost.data.internal)
-		if (internalPost.data.internal && internalPost.data.internal.sentiWaterworks) {
-			let resources = await aclClient.findResources(user.uuid, '00000000-0000-0000-0000-000000000000', sentiAclResourceType.device, sentiAclPriviledge.device.read)
-			if (resources.length === 0) {
-				res.status(404).json([])
-				return
-			}
-			let deviceUUIDs = (resources.length > 0) ? resources.map(item => { return item.uuid }) : []
-			internalPost.data.internal.sentiWaterworks.devices = deviceUUIDs
+// router.get('/onboarding/userfix/:uuid', async (req, res) => {
+// 	if (authClient.getStoredToken() === false) {
+// 		let login = await authClient.login(process.env.SENTIUSER, process.env.SENTIPASS)
+// 		authClient.setStoredToken(login.token)
+// 	}
+// 	if (await authClient.getTokenLease(authClient.getStoredToken()) === false) {
+// 		authClient.setStoredToken((await authClient.login(process.env.SENTIUSER, process.env.SENTIPASS)).token)
+// 	}
+// 	coreAPI.setHeader('Authorization', 'Bearer ' + authClient.getStoredToken())
+// 	dataBrokerAPI.setHeader('Authorization', 'Bearer ' + authClient.getStoredToken())
+// 	if (req.headers['wlhost']) {
+// 		console.log('SET WLHOST', req.headers['wlhost'])
+// 		coreAPI.setHeader('wlhost', req.headers['wlhost'])
+// 		dataBrokerAPI.setHeader('wlhost', req.headers['wlhost'])
+// 	}
+// 	let userPost = await coreAPI.get(`/v2/entity/users/${req.params.uuid}`)
+// 	if(userPost.ok === false) {
+// 		res.status(userPost.status).json()
+// 		return
+// 	}
+// 	let users = []
+// 	await userPost.data.reduce(async (promise, user) => {
+// 		// This line will wait for the last async function to finish.
+// 		// The first iteration uses an already resolved Promise
+// 		// so, it will immediately continue.
+// 		await promise;
+// 		console.log(user.uuid)
+// 		let internalPost = await coreAPI.get(`/v2/entity/user/${user.uuid}/internal`)
+// 		if(internalPost.ok === false) {
+// 			res.status(internalPost.status).json()
+// 			return
+// 		}
+// 		console.log(internalPost.data.internal)
+// 		if (internalPost.data.internal && internalPost.data.internal.sentiWaterworks) {
+// 			let resources = await aclClient.findResources(user.uuid, '00000000-0000-0000-0000-000000000000', sentiAclResourceType.device, sentiAclPriviledge.device.read)
+// 			if (resources.length === 0) {
+// 				res.status(404).json([])
+// 				return
+// 			}
+// 			let deviceUUIDs = (resources.length > 0) ? resources.map(item => { return item.uuid }) : []
+// 			internalPost.data.internal.sentiWaterworks.devices = deviceUUIDs
 
-			let userInternalPost = await coreAPI.put('/v2/entity/user/' + user.uuid + '/internal', internalPost.data.internal)
-			if(userInternalPost.ok === false) {
-				console.log('error on ', internalPost.data)
-			}
+// 			let userInternalPost = await coreAPI.put('/v2/entity/user/' + user.uuid + '/internal', internalPost.data.internal)
+// 			if(userInternalPost.ok === false) {
+// 				console.log('error on ', internalPost.data)
+// 			}
 	
-			users.push(internalPost.data)
-		}
-	}, Promise.resolve());
-
-
-	res.status(200).json(users);
-})
+// 			users.push(internalPost.data)
+// 		}
+// 	}, Promise.resolve());
+// 	res.status(200).json(users);
+// })
 router.post('/onboarding/user/:installationuuid', async (req, res) => {
 	// Check installation
 	let select = `SELECT * FROM installations I WHERE I.uuid = ? AND I.state = ?`
