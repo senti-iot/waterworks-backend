385fb1d4ec42f72bef1f09cd76f49f76d8162572
Andrei <at@senti.dk>
2021-04-30 20:23:34 +0200

    update the sql

--- test/travis.sql
+++ test/travis.sql
@@ -1,55 +1,55 @@
 CREATE DATABASE `waterworksdev` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_danish_ci */;
 
-CREATE TABLE waterworksdev.installation (
-	id INT auto_increment NOT NULL,
-	uuid varchar(36) NOT NULL,
-	address varchar(255) NULL,
-	orgUUID VARCHAR(255) NULL,
-	state SMALLINT DEFAULT 0 NOT NULL,
-	adults SMALLINT NULL,
-	children SMALLINT NULL,
-	deleted BOOLEAN DEFAULT 0 NOT NULL ,
-	CONSTRAINT `inst_pk` PRIMARY KEY (id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_danish_ci;
+
+CREATE TABLE `installation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(36) COLLATE utf8mb4_danish_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `orgUUID` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `state` smallint(6) NOT NULL DEFAULT '0',
+  `deleted` tinyint(1) DEFAULT '0',
+  `operation` tinyint(1) NOT NULL DEFAULT '0',
+  `moving` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `inst_uuid` (`uuid`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_danish_ci;
 
 CREATE INDEX inst_uuid USING BTREE on waterworksdev.installation(`uuid`);
 
 
 
-CREATE TABLE waterworksdev.instDevice (
-	id INT auto_increment NOT NULL,
-	uuid varchar(36) NOT NULL,
-	startDate DATETIME NOT NULL,
-	endDate DATETIME,
-	deviceUUID varchar(36) NOT NULL,
-	instUUID varchar(36) NOT NULL,
-	deleted BOOLEAN DEFAULT 0 NOT NULL ,
-	CONSTRAINT `instDevice_pk` PRIMARY KEY (id),
-	CONSTRAINT `instDevice_installation_fk` FOREIGN KEY (instUUID) references installation(uuid)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_danish_ci;
+-- waterworksdev.instDevice definition
+
+CREATE TABLE `instDevice` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(36) COLLATE utf8mb4_danish_ci NOT NULL,
+  `startDate` datetime NOT NULL,
+  `endDate` datetime DEFAULT NULL,
+  `deviceUUID` varchar(36) COLLATE utf8mb4_danish_ci NOT NULL,
+  `instUUID` varchar(36) COLLATE utf8mb4_danish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `instDevice_installation_fk` (`instUUID`),
+  KEY `instDevice_uuid` (`uuid`) USING BTREE,
+  CONSTRAINT `instDevice_installation_fk` FOREIGN KEY (`instUUID`) REFERENCES `installation` (`uuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_danish_ci;
 
 CREATE INDEX instDevice_uuid USING BTREE on waterworksdev.instDevice(`uuid`);
 
+-- waterworksdev.instUser definition
 
-create table waterworksdev.instUser (
-	id INT auto_increment NOT NULL,
-	uuid varchar(36) NOT NULL,
-	startDate DATETIME NOT NULL,
-	endDate DATETIME,
-	userUUID varchar(36) NOT NULL,
-	instUUID varchar(36) NOT NULL,
-	deleted BOOLEAN DEFAULT 0 NOT NULL ,
-	CONSTRAINT `instUser_pk` PRIMARY KEY (id),
-	CONSTRAINT `instUser_installation_fk` FOREIGN KEY (instUUID) references installation(uuid)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_danish_ci;
+CREATE TABLE `instUser` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(36) COLLATE utf8mb4_danish_ci NOT NULL,
+  `startDate` datetime NOT NULL,
+  `endDate` datetime DEFAULT NULL,
+  `userUUID` varchar(36) COLLATE utf8mb4_danish_ci NOT NULL,
+  `instUUID` varchar(36) COLLATE utf8mb4_danish_ci NOT NULL,
+  `adults` int(11) NOT NULL DEFAULT '1',
+  `children` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `instUser_installation_fk` (`instUUID`),
+  KEY `instUser_uuid` (`uuid`) USING BTREE,
+  CONSTRAINT `instUser_installation_fk` FOREIGN KEY (`instUUID`) REFERENCES `installation` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_danish_ci;
 
 CREATE INDEX instUser_uuid USING BTREE on waterworksdev.instUser(`uuid`);
