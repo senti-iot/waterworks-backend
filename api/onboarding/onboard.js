7d88c73b0dfd25de276c598a63e5fcac30a49cd1
Andrei Tudor <andrei.tudor27@gmail.com>
2021-11-21 00:41:43 +0100

    Complete onboard

--- api/onboarding/onboard.js
+++ api/onboarding/onboard.js
@@ -18,10 +18,10 @@ router.post(`/v3/onboard/update-installation-address`, async (req, res) => {
 	if (oService) {
 		let updInst = await oService.updateInstAddress(body)
 		if (updInst) {
-			return res.status(200).json({"Result": true})
+			return res.status(200).json({ "Result": true })
 		}
 
-		return res.status(500).json({"Result": false})
+		return res.status(500).json({ "Result": false })
 	}
 	else {
 		return res.status(500).json({ "Error": "oService is closed." })
@@ -64,7 +64,7 @@ router.post(`/v3/onboard/create-senti-user`, async (req, res) => {
 		}
 	}
 	else {
-		return res.status(500).json({"Error": "oService is closed."})
+		return res.status(500).json({ "Error": "oService is closed." })
 	}
 })
 
@@ -87,5 +87,37 @@ router.post(`/v3/onboard/create-waterworks-user`, async (req, res) => {
 	}
 })
 
+/**
+ *
+ */
+router.post(`/v3/onboard/complete`, async (req, res) => {
+	let body = req.body
+	let sUserBody = body.sUserBody
+	let wUserBody = body.wUserBody
+	let instBody = body.instBody
+	if (oService) {
+		//Create Senti User or check if it is already created
+		let user = await oService.findSentiUser(sUserBody)
+		if (!user) {
+			user = await oService.createSentiUser(userBody)
+		}
+
+		if (user) {
+			//If the user exists, create the Installation User
+			wUserBody.userUUID = user.uuid
+			let wUser = await oService.createWaterworksUser(wUserBody)
+			if (wUser) {
+				//Update the installation
+				let inst = await oService.updateInstallation(instBody)
+			}
+			else {
+				res.status(500).json({ "Error": "Installation User failed to be created." })
+			}
+		}
+		else {
+			res.status(500).json({ "Error": "User failed to be created"})
+		}
+	}
+})
 
 module.exports = router
